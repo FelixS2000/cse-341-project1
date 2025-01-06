12d2d51e37d26f0c82fd0ec2ef143666ff939cef
Felix Samuel Guzman Ceri <113263790+FelixS2000@users.noreply.github.com>
2025-01-06 23:54:21 +0000

    Fixing server.js

--- server.js
+++ server.js
@@ -3,6 +3,4 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => (
-  console.log(`Running on port ${port}`)
-));
+app.listen(port, () => {console.log(`Running on port ${port}`)});
